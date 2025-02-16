623eabb3df706fe3679c9816bda0b9ecc931750c
Misha Gupta <mishagupta@Mishas-MacBook-Air-9.local>
2025-02-16 00:56:37 -0500

    maven

---
+++ src/main/java/com/mg/apidaaalumni/ApidaaalumniApplication.java
@@ -0,0 +1,13 @@
+package com.mg.apidaaalumni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidaaalumniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidaaalumniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mg/apidaaalumni/ApidaaalumniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.apidaaalumni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidaaalumniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
